e8e755268e75b2ae0593f231075c4d66a9e70ec1
S GAYATHRI IYER <81139359+SGayathri05@users.noreply.github.com>
2023-08-19 20:14:15 +0530

    Update Self_Join.sql

--- Self_Join.sql
+++ Self_Join.sql
@@ -3,4 +3,14 @@
 How many stops are in the database.
 */
 select count(name) from stops
-  
+
+# 2
+/*
+Find the id value for the stop 'Craiglockhart'
+*/
+select id from stops where name = 'Craiglockhart'
+
+# 3
+/*
+How many stops are in the database.
+*/
